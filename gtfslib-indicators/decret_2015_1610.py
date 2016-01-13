26e270b123fcff294b19811a1a34d5b63d9e6b62
Laurent Grégoire <laurent.gregoire@gmail.com>
2016-01-13 17:47:30 +0100

    Version plus "pythonesque" de l'indicateur.
    See issue #1.

--- gtfslib-indicators/decret_2015_1610.py
+++ gtfslib-indicators/decret_2015_1610.py
@@ -13,66 +13,79 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with gtfslib-python.  If not, see <http://www.gnu.org/licenses/>.
-from gtfslib.utils import gtfstime, fmttime
 """
 @author: Laurent GRÉGOIRE <laurent.gregoire@mecatran.com>
 """
-"""
-Cette fonction teste un réseau par rapport au décret n° 2015-1610 du 8 décembre 2015
-relatif aux critères d'espacement moyen des arrêts et de variation de la fréquence de
-passage des services réguliers de transport public routier urbain de personnes.
 
-Pour plus d'informations:
-http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000031589954
-"""
-
+from _collections import defaultdict
 import logging
 
 from sqlalchemy.sql.functions import func
 
 from gtfslib.dao import Dao
 from gtfslib.model import CalendarDate, Calendar, Trip, StopTime
+from gtfslib.utils import gtfstime, fmttime
 
+"""
+Cette fonction teste un ensemble de voyages (trips) d'un réseau, d'une ligne
+ou d'une collection de lignes, par rapport au décret n° 2015-1610 du 8 décembre 2015
+relatif aux critères d'espacement moyen des arrêts et de variation de la fréquence de
+passage des services réguliers de transport public routier urbain de personnes.
 
-def decret_2015_1610(dao):
+Pour plus d'informations:
+http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000031589954
+"""
+def decret_2015_1610(trips):
 
     print("Calcul de l'espacement moyen des arrêts...")
     espacement_moyen = 0
     w_esp = 0
-    for trip in dao.trips(prefetch_stop_times=True):
-        ndays = len(trip.calendar.dates)
+    for trip in trips:
+        # Note: on pondère par le nombre de jours chaque voyage est applicable.
+        # Ceci permet de prendre en compte la fréquence: par exemple, la distance
+        # d'un intervalle entre deux arrêt actif le lundi uniquement sera pondéré
+        # 5 fois moins qu'un autre intervalle actif du lundi au vendredi.
+        n_jours = len(trip.calendar.dates)
         for stoptime1, stoptime2 in trip.hops():
-            espacement_moyen += (stoptime2.shape_dist_traveled - stoptime1.shape_dist_traveled) * ndays
-            w_esp += ndays
+            espacement_moyen += (stoptime2.shape_dist_traveled - stoptime1.shape_dist_traveled) * n_jours
+            w_esp += n_jours
     espacement_moyen /= w_esp
     print("L'espacement moyen entre arrêt du réseau est de %.2f mètres (max 500m)." % espacement_moyen)
 
     print("Calcul du jour ayant la fréquence en voyage la plus élevée...")
-    total = func.count(Trip.trip_id)
-    date, nombre_voyages = dao.session() \
-                .query(CalendarDate.date, total) \
-                .join(Calendar).join(Trip) \
-                .group_by(CalendarDate.date) \
-                .order_by(total.desc()) \
-                .first()
-    print("Le jour ayant le nombre de voyage le plus élevé est le %s, avec %d voyages." % (date, nombre_voyages))
+    frequences = defaultdict(lambda: 0)
+    for trip in trips:
+        for date in trip.calendar.dates:
+            frequences[date] += 1
+    date_max = None
+    freq_max = 0
+    for date, frequence in frequences.items():
+        if frequence > freq_max:
+            freq_max = frequence
+            date_max = date
+    print("Le jour ayant le nombre de voyage le plus élevé est le %s, avec %d voyages." % (date_max.as_date(), freq_max))
 
     print("Calcul des fréquences sur la plage horaire 8h - 19h...")
     # TODO Est-ce que ce calcul est correct? Le décret est pas clair.
-    frequences = [ 0 for minute in xrange(0, 20 * 60) ]
-    for trip in dao.trips(calendar_fltr=(func.date(CalendarDate.date) == date), prefetch_stop_times=True):
-        minute_depart = trip.stop_times[0].departure_time / 60
-        minute_arrivee = trip.stop_times[-1].arrival_time / 60
-        for minute in xrange(minute_depart, minute_arrivee + 1):
+    # On calcule le nombre de voyages actifs pendant chaque minute.
+    frequences = [ 0 for minute in range(0, 20 * 60) ]
+    for trip in trips:
+        if date_max not in trip.calendar.dates:
+            continue
+        minute_depart = trip.stop_times[0].departure_time // 60
+        minute_arrivee = trip.stop_times[-1].arrival_time // 60
+        for minute in range(minute_depart, minute_arrivee + 1):
             if minute >= 8 * 60 and minute < 20 * 60:
                 frequences[minute] += 1
     frequence_min = 99999999999
     minute_min = 0
     frequence_max = 0
     minute_max = 0
-    for minute in xrange(8 * 60, 19 * 60):
+    # La fréquence horaire min/max est calculé en moyenne glissante
+    # sur une heure en sommant les fréquences par minute.
+    for minute in range(8 * 60, 19 * 60):
         freq = 0
-        for delta_minute in xrange(0, 60):
+        for delta_minute in range(0, 60):
             freq += frequences[minute + delta_minute]
         if freq > frequence_max:
             frequence_max = freq
@@ -80,8 +93,8 @@ def decret_2015_1610(dao):
         if freq < frequence_min:
             frequence_min = freq
             minute_min = minute
-    print("La fréquence minimale est de %d voyages, entre %s et %s." % (frequence_min, fmttime(minute_min * 60), fmttime((minute_min + 60) * 60)))
-    print("La fréquence maximale est de %d voyages, entre %s et %s." % (frequence_max, fmttime(minute_max * 60), fmttime((minute_max + 60) * 60)))
+    print("La fréquence minimale est de %.2f voyages/heure, entre %s et %s." % (frequence_min / 60.0, fmttime(minute_min * 60), fmttime((minute_min + 60) * 60)))
+    print("La fréquence maximale est de %.2f voyages/heure, entre %s et %s." % (frequence_max / 60.0, fmttime(minute_max * 60), fmttime((minute_max + 60) * 60)))
     ratio_frequence = frequence_max / float(frequence_min)
     print("Le ratio entre fréquence max et min est de %.3f (max 2.5)." % ratio_frequence)
 
@@ -93,8 +106,13 @@ def decret_2015_1610(dao):
 if __name__ == '__main__':
     # TODO Make a proper main
     logging.basicConfig(level=logging.INFO)
+
     dao = Dao("decret.sqlite")
     #dao.delete_feed("")
     #dao.load_gtfs("gtfs.zip")
-    urbain = decret_2015_1610(dao)
+
+    print("Chargement des données...")
+    # trips = dao.trips(fltr=(Trip.route_id == 'A1'), prefetch_stop_times=True, prefetch_calendars=True)
+    trips = dao.trips(fltr=(Trip.route_id == 'A1'), prefetch_stop_times=True, prefetch_calendars=True)
+    urbain = decret_2015_1610(trips)
     print("urbain=%s" % urbain)
